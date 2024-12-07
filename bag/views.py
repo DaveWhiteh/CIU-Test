9fed118ecd439b260e128c50bad9735ed5d2e196
DaveWhiteh <david_whiteh@msn.com>
2024-12-07 20:48:52 +0000

    Added notification css, shopping bag preview to toasts

--- bag/views.py
+++ bag/views.py
@@ -13,13 +13,14 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.model_name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.model_name} to your bag')
@@ -34,12 +35,15 @@ def remove_from_bag(request, item_id):
     """ Remove the item from the shopping bag """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.model_name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
